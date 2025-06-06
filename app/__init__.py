2c1ad67bab33df274b2bf5719fb99a6a48f286c0
Aravinth-Rheo <aravinth.m@rheo.ai>
2025-06-06 15:06:43 +0530

    add ssl

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
-import ssl
 
 load_dotenv()  
 
@@ -15,12 +14,7 @@ def create_app():
     MONGO_URI = os.getenv("MONGO_CLIENT")
 
     global mongo_client, db
-    mongo_client = MongoClient(
-        MONGO_URI,
-        tls=True,  
-        tlsAllowInvalidCertificates=True, 
-        tlsCAFile=None  
-    )
+    mongo_client = MongoClient(MONGO_URI)
     db = mongo_client.movies 
 
     from .routes import register_blueprints
